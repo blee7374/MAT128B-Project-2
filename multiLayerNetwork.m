58dbf583e1f6f452c72c94b41f70a76ef385f809
alexjbutterfield <36579033+alexjbutterfield@users.noreply.github.com>
2018-03-13 01:46:07 -0700

    Add files via upload
    multiLayerNetwork to be called for each digit to be processed

---
+++ multiLayerNetwork.m
@@ -0,0 +1,14 @@
+function [outputArg1,outputArg2] = multiLayerNetwork(inputArg1,inputArg2)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+%take inputs
+input = [1, 2, 3, 4, 5];
+
+
+
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
